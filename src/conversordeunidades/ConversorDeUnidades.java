b3cffc8660a822b24de66ad72383dee9965d5803
Nicolas Martin <nicolasmartin89@gmail.com>
2023-07-10 19:54:49 -0300

    Version 1.2 del Conversor

--- src/conversordeunidades/ConversorDeUnidades.java
+++ src/conversordeunidades/ConversorDeUnidades.java
@@ -10,14 +10,27 @@ public class ConversorDeUnidades {
         if (menu.seleccion.equalsIgnoreCase("Conversor de Monedas")) {
             MenuConversorMoneda menuConversor = new MenuConversorMoneda();
             ServiciosConversorMoneda servConversor = new ServiciosConversorMoneda();
+            boolean validado = false;
 
-            String montoString = JOptionPane.showInputDialog("Ingrese el monto a convertir:");
-            double montoARS = Double.parseDouble(montoString);
-
-            servConversor.calcularMonto(montoARS, menuConversor);
-
+            while (!validado) {
+                try {
+                    String montoString = JOptionPane.showInputDialog("Ingrese el monto a convertir:");
+                    double montoARS = Double.parseDouble(montoString);
+                    servConversor.calcularMonto(montoARS, menuConversor);
+                    int opcion = JOptionPane.showConfirmDialog(null, "Desea continuar?");
+                    if (opcion == JOptionPane.YES_OPTION) {
+                       
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Programa Finalizado");
+                        validado = true;
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Introduciste un numero invalido, introducelo nuevamente:");
+                }
+            }
         } else if (menu.seleccion.equalsIgnoreCase("Conversor de temperaturas")) {
-            JOptionPane.showMessageDialog(null, "Funcionalidad no implementada...");
+            JOptionPane.showMessageDialog(null, "Funcionalidad no implementada...Saliendo del programa.");
         }
+
     }
 }
